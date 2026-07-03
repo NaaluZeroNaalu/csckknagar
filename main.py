4a0d73ec0d8afe83869de8987b54d17e8e712354
NaaluZeroNaalu <ajithruflex1210gmail.com>
2026-07-03 12:30:45 +0530

    connect to database

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 import json
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine, text
 
-# load_dotenv()
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 app = FastAPI()
 
 app.add_middleware(
@@ -15,7 +19,23 @@ app.add_middleware(
     allow_headers=["*"],    
 )
 
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+)
+
+
 @app.get("/")
 def index():
   
     return {"status":"API is Running"}
+
+@app.get("/students")
+def get_students():
+    with engine.connect() as conn:
+        result = conn.execution_options(stream_results=True).execute(
+            text("SELECT * FROM STUDENTS")
+        )
+        return result.mappings().all()
