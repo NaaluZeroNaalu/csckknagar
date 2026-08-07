2bbbadd33f8e98571dc0d375a463d4d8ade56351
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2026-08-07 15:21:08 +0530

    shuba mail python file

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("welcome")
+pages = {
+
+    "REPORTS":[
+        st.Page("pro.py",title="Send Mail ",icon=":material/view_timeline:"),
+        st.Page("test.py",title="Check",icon=":material/home:"),
+
+    ]
+}
+
+
+st.navigation(pages).run()
