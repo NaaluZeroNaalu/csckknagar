4793ece687641b8d9d89af0569910b6d867d6d0e
NaaluZeroNaalu <126253328+NaaluZeroNaalu@users.noreply.github.com>
2026-08-07 15:24:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ pages = {
 
     "REPORTS":[
         st.Page("pro.py",title="Send Mail ",icon=":material/view_timeline:"),
-        st.Page("test.py",title="Check",icon=":material/home:"),
+        st.Page("Test.py",title="Check",icon=":material/home:"),
 
     ]
 }
